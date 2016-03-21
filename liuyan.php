a68f79107bee3ebce372763546a0fe4e00ec3326
ybupengwang <527534532@qq.com>
2016-03-21 13:27:13 +0800

    Create liuyan.php

---
+++ liuyan.php
@@ -0,0 +1,39 @@
+<html>
+<body>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<table width="678" align="center">
+<tr>
+<td colspan="2"><h1>留言本</h1></td>
+</tr>
+<tr>
+<td width="586"><a href="index.php">首页</a> | <a href="liuyan.php">留言</a></td>
+</tr>
+</table>
+<table align="center" width="678">
+<tr>
+<td>
+<form name="form1" method="post" action="post.php">
+<p>
+Name：
+<input name="name" type="text" id="name">
+</p>
+<p>Email：<input type="test" name="email" id="email"></p>
+<p>
+留言：
+</p>
+<p>
+<textarea name="content" id="content" cols="45" rows="5"></textarea>
+</p>
+<p>
+<input type="submit" name="button" id="button" value="提交">
+<input type="reset" name="button2" id="button2" value="重置">
+</p></form>
+</td>
+</tr>
+</table>
+</body>
+
+
+
+</html>
+
