4b1f9f0d8322ec3ab7d86e9d933967dc91b82177
wangpeng <527534532@qq.com>
2016-03-23 12:35:54 +0800

    修改了文件编写规范

--- index.php
+++ index.php
@@ -7,28 +7,28 @@ $resule = mysql_query($sql,$q);
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <body>
 <table width="678" align="center">
-<tr>
-<td colspan="2"><h1>留言本</h1></td>
-</tr>
-<tr>
-<td width="586"><a href="index.php">首页</a> | <a href="liuyan.php">留言</a></td>
-</tr>
+	<tr>
+		<td colspan="2"><h1>留言本</h1></td>
+	</tr>
+	<tr>
+		<td width="586"><a href="index.php">首页</a> | <a href="liuyan.php">留言</a></td>
+	</tr>
 </table>
 <p>
 <?
-while($row=mysql_fetch_array($resule))
+	while($row=mysql_fetch_array($resule))
 {
 ?>
 </p>
 <table width="678" border="1" align="center" cellpadding="1" cellspacing="1">
-<tr>
-<td width="178">Name:<? echo $row[1] ?></td>
-<td width="223">Email:<? echo $row[2] ?></td>
-</tr>
-<tr>
-<td colspan="4"><? echo $row[3] ?></td>
-</tr>
-<tr>
+	<tr>
+		<td width="178">Name:<? echo $row[1] ?></td>
+		<td width="223">Email:<? echo $row[2] ?></td>
+	</tr>
+	<tr>
+		<td colspan="4"><? echo $row[3] ?></td>
+	</tr>
+	
 </table>
 <?
 }
--- post.php
+++ post.php
@@ -4,8 +4,7 @@ include("config.php");
 $name= $_POST['name'];
 $email= $_POST['email'];
 $patch = $_POST['content'];
-$content = str_replace("
-","<br />",$patch);
+$content = str_replace("","<br />",$patch);
 $sql = "insert into content (name,email,content) values ('$name','$email','$content')";
 mysql_query($sql);
 echo "<script>alert('提交成功！返回首页。');location.href='index.php';</script>";
