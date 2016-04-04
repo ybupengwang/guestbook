aad1a367dd09c07ae021f8cd91587ed6723c1f32
ybupengwang <527534532@qq.com>
2016-04-04 13:00:39 +0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$q = mysql_connect("127.0.0.1:3306","root","");
-if(!$q)
-{
-die('Could not connect: ' . mysql_error());
-}
-mysql_query("set names utf8"); //以utf8读取数据
-mysql_select_db("guestbook",$q); //数据库
-?>
